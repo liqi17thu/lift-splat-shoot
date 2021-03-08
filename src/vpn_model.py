c01d6cc544d588e096f76ec8daa2855b149993b9
Qi Li <liqi17thu@gmail.com>
2021-03-08 11:06:08 +0800

    temporal VPN

--- src/vpn_model.py
+++ src/vpn_model.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
 
+from .homography import bilinear_sampler
+from .tools import plane_grid_2d, get_rot_2d, cam_to_pixel
+
 from efficientnet_pytorch import EfficientNet
 from torchvision.models.resnet import resnet18
 
@@ -172,5 +175,66 @@ class VPNet(nn.Module):
         x = self.get_cam_feats(x)
         topdown = self.view_fusion(x)
         topdown = self.up_sampler(topdown)
+        return self.bevencode(topdown)
+
+
+class TemporalVPNet(nn.Module):
+    def __init__(self, xbound, ybound, outC, camC=64, instance_seg=True, embedded_dim=16):
+        super(TemporalVPNet, self).__init__()
+        self.xbound = xbound
+        self.ybound = ybound
+        self.camC = camC
+        self.downsample = 16
+
+        self.camencode = CamEncode(camC)
+        self.view_fusion = ViewFusionModule(fv_size=(8, 22), bv_size=(40, 80))
+        self.up_sampler = nn.Upsample(scale_factor=5, mode='bilinear', align_corners=True)
+        self.bevencode = BevEncode(inC=camC, outC=outC, instance_seg=instance_seg, embedded_dim=embedded_dim)
+
+    def get_cam_feats(self, x):
+        """Return B x T x N x H/downsample x W/downsample x C
+        """
+        B, T, N, C, imH, imW = x.shape
+
+        x = x.view(B*T*N, C, imH, imW)
+        x = self.camencode(x)
+        x = x.view(B, T, N, self.camC, imH//self.downsample, imW//self.downsample)
+        return x
+
+    def temporal_fusion(self, topdown, translation, yaw):
+        B, T, C, H, W = topdown.shape
+
+        if T == 1:
+            return topdown[:, 0]
+
+        grid = plane_grid_2d(self.xbound, self.ybound).view(1, 1, 2, H*W).repeat(B, T-1, 1, 1)
+        rot0 = get_rot_2d(yaw[:, 1:])
+        trans0 = translation[:, 1:, :2].view(B, T-1, 2, 1)
+        rot1 = get_rot_2d(yaw[:, 0].view(B, 1).repeat(1, T-1))
+        trans1 = translation[:, 0, :2].view(B, 1, 2, 1).repeat(1, T-1, 1, 1)
+        grid = rot1.transpose(2, 3) @ grid
+        grid = grid + trans1
+        grid = grid - trans0
+        grid = rot0 @ grid
+        grid = grid.view(B*(T-1), 2, H, W).permute(0, 2, 3, 1).contiguous()
+        grid = cam_to_pixel(grid, self.xbound, self.ybound)
+        topdown = topdown.permute(0, 1, 3, 4, 2).contiguous()
+        prev_topdown = topdown[:, 1:]
+        warped_prev_topdown = bilinear_sampler(prev_topdown.reshape(B*(T-1), H, W, C), grid).view(B, T-1, H, W, C)
+        topdown = torch.cat([topdown[:, 0].unsqueeze(1), warped_prev_topdown], axis=1)
+        topdown = topdown.view(B, T, H, W, C)
+        topdown = topdown.max(1)[0]
+        topdown = topdown.permute(0, 3, 1, 2).contiguous()
+        return topdown
+
+    def forward(self, x, rots, trans, intrins, post_rots, post_trans, translation, yaw_pitch_roll):
+        x = self.get_cam_feats(x)
+        B, T, N, C, h, w = x.shape
+        x = x.view(B*T, N, C, h, w)
+        topdown = self.view_fusion(x)
+        topdown = self.up_sampler(topdown)
+        _, C, H, W = topdown.shape
+        topdown = topdown.view(B, T, C, H, W)
+        topdown = self.temporal_fusion(topdown, translation, yaw_pitch_roll[..., 0])
         return self.bevencode(topdown)
 
